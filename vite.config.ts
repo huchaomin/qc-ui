d7d01c3ace9d80d6873ba8dd7517fc28a8f00c50
peter <peter@qingcongai.com>
2025-06-03 10:24:53 +0800

    lightningcss

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,23 @@ import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import browserslist from 'browserslist'
+import { browserslistToTargets } from 'lightningcss'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    cssMinify: 'lightningcss',
+  },
+  css: {
+    lightningcss: {
+      // https://cn.vitejs.dev/config/shared-options#css-lightningcss
+      targets: browserslistToTargets(browserslist('>= 0.25%')),
+    },
+    transformer: 'lightningcss',
+  },
   plugins: [
     vue(),
     vueJsx(),
