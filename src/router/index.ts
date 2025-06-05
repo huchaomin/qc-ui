a7b0d2f3386b6d28d95ff41e3f4f8225f85fa450
peter <peter@qingcongai.com>
2025-06-05 11:00:57 +0800

    删除无效的文件

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      component: HomeView,
-      name: 'home',
-      path: '/',
-    },
-    {
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: async () => import('../views/AboutView.vue'),
-      name: 'about',
-      path: '/about',
-    },
   ],
 })
 
