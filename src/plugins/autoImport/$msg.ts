52999f796532fb335f61eaae7bf212cb04e2d70c
peter <peter@qingcongai.com>
2025-06-09 18:03:07 +0800

    $msg

---
+++ src/plugins/autoImport/$msg.ts
@@ -0,0 +1,56 @@
+import type { MessageInstance, MessageOptions, MessagePluginType } from 'tdesign-vue-next'
+
+// MessageThemeList
+enum MessageCreateType {
+  error = 'error',
+  info = 'info',
+  loading = 'loading',
+  question = 'question',
+  success = 'success',
+  warning = 'warning',
+}
+type CreateMessageFnType = (
+  content: MessageOptions['content'],
+  options?: Omit<MessageOptions, 'content' | 'theme'>,
+) => Promise<MessageInstance>
+type CreateMessageType = CreateMessageFnType &
+  {
+    [value in MessageCreateType]: CreateMessageFnType
+  } &
+  {
+    close: MessagePluginType['close']
+    closeAll: MessagePluginType['closeAll']
+  }
+
+async function create(
+  type: `${MessageCreateType}`,
+  content: Parameters<CreateMessageFnType>[0],
+  options: Parameters<CreateMessageFnType>[1],
+) {
+  const obj = {
+    ...(options ?? {}),
+  }
+  return MessagePlugin[type]({
+    content,
+    ...obj,
+  })
+}
+
+const createMessage: CreateMessageType = async function (...arg) {
+  return create('success', ...arg)
+} as CreateMessageType
+Object.values(MessageCreateType).forEach((type) => {
+  createMessage[type] = async (...arg) => {
+    return create(type, ...arg)
+  }
+})
+
+createMessage.closeAll = () => {
+  return MessagePlugin.closeAll()
+}
+
+createMessage.close = (...arg: Parameters<MessagePluginType['close']>) => {
+  return MessagePlugin.close(...arg)
+}
+
+export default createMessage
