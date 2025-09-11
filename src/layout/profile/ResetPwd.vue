5ded32fff79c4ada437efc673c2ad9eb22fed8ae
peterhu <peter@qcznai.com>
2025-09-11 11:32:25 +0800

    个人中心修改密码

--- src/layout/profile/ResetPwd.vue
+++ src/layout/profile/ResetPwd.vue
@@ -1,9 +1,88 @@
 <script setup lang="ts">
-debugger
+const route = useRoute()
+const formData = reactive({
+  confirmPassword: '',
+  newPassword: '',
+  oldPassword: '',
+})
+const formItems: FormItemType[] = [
+  {
+    _label: '旧密码',
+    _required: true,
+    model: 'oldPassword',
+    type: 'password',
+  },
+  {
+    _label: '新密码',
+    _required: true,
+    _rules: [
+      {
+        message:
+          '密码必须为8位或8位以上的必须包含(数字、大写字母、小写字母)的组合',
+        pattern: passwordReg,
+      },
+    ],
+    model: 'newPassword',
+    type: 'password',
+  },
+  {
+    _label: '确认密码',
+    _required: true,
+    _rules: [
+      {
+        validator: (val) => {
+          if (val === formData.newPassword) {
+            return true
+          }
+
+          return {
+            message: '两次输入的密码不一致',
+            result: false,
+          }
+        },
+      },
+    ],
+    model: 'confirmPassword',
+    type: 'password',
+  },
+  {
+    _label: ' ',
+    slot: 'footer',
+  },
+]
+const formRef = useTemplateRef('form')
+
+function handleClose() {
+  useRecentRoutersStore().close(route.name)
+}
+
+async function handleSubmit() {
+  await formRef.value!.validate()
+  await alovaInst.Put('system/user/profile/updatePwd', undefined, {
+    meta: {
+      useLoading: '保存中...',
+      useSuccessMsg: true,
+    },
+    params: {
+      newPassword: formData.newPassword,
+      oldPassword: formData.oldPassword,
+    },
+  })
+  useLoginStore().logout()
+}
 </script>
 
 <template>
-  <div>
-    <h1>ResetPwd</h1>
-  </div>
+  <TForm
+    ref="form"
+    :data="formData"
+    :items="formItems"
+    layout="vertical"
+    label-align="right"
+  >
+    <template #footer>
+      <TButton @click="handleSubmit">保存</TButton>
+      <TButton theme="default" class="!ml-4" @click="handleClose">关闭</TButton>
+    </template>
+  </TForm>
 </template>
