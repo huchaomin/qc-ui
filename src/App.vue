b62604daeb0232c114d8b9034d2fde4e6c6d4092
peterhu <peter@qcznai.com>
2025-09-11 16:46:28 +0800

    TWatermark

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,27 @@
 import type { GlobalConfigProvider } from 'tdesign-vue-next'
 
 const globalConfig: GlobalConfigProvider = {}
+const userStore = useUserStore()
 </script>
 
 <template>
   <TConfigProvider :global-config="globalConfig">
     <RouterView></RouterView>
+    <TWatermark
+      class="pointer-events-none !absolute top-0 h-full"
+      :watermark-content="[
+        { text: userStore.userInfo.userName ?? '' },
+        {
+          text: userStore.userInfo.nickName ?? '',
+        },
+      ]"
+      :y="100"
+      :x="50"
+      :removable="false"
+      :line-space="24"
+      :width="120"
+      :height="60"
+    >
+    </TWatermark>
   </TConfigProvider>
 </template>
