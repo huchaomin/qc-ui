4564e54bf2411eabd97a097456edc54533ebac6d
peterhu <peter@qcznai.com>
2025-09-15 09:22:08 +0800

    卸载 @visactor/vue-vtable

--- src/layout/Login.vue
+++ src/layout/Login.vue
@@ -1,5 +1,113 @@
+<script setup lang="ts">
+import type { LoginData } from '@/store/modules/login'
+import captcha from '@/plugins/captcha'
+import LoginBg from './modules/LoginBg.vue'
+
+const loginStore = useLoginStore()
+const router = useRouter()
+const appName = import.meta.env.VITE_APP_NAME
+const formData = reactive<LoginData>({
+  password: '',
+  rememberMe: true,
+  username: '',
+})
+
+async function initLoginData() {
+  formData.username = await aesDecrypt(loginStore.username)
+  formData.password = await aesDecrypt(loginStore.password)
+}
+
+if (loginStore.rememberMe) {
+  initLoginData()
+}
+
+const useCaptcha = import.meta.env.VITE_USE_CAPTCHA
+
+async function loginSubmit(formData: LoginData) {
+  await loginStore.login(formData)
+
+  if (formData.rememberMe) {
+    await loginStore.storeLoginData({
+      password: formData.password,
+      rememberMe: formData.rememberMe,
+      username: formData.username,
+    })
+  } else {
+    loginStore.clearLoginData()
+  }
+
+  router.replace({ name: 'Index' })
+}
+
+async function onSubmit() {
+  if (formData.username === '') {
+    $msg.error('请输入账户名')
+    return
+  }
+
+  if (formData.password === '') {
+    $msg.error('请输入密码')
+    return
+  }
+
+  if (useCaptcha) {
+    formData.code = await captcha()
+    loginSubmit(formData)
+  } else {
+    loginSubmit(formData)
+  }
+}
+
+const formItems: FormItemType[] = [
+  {
+    _label: '账户',
+    clearable: false,
+    model: 'username',
+    placeholder: '账号',
+    prefixIcon(h) {
+      return h(resolveComponent('icon'), {
+        icon: 'mdi:shield-account-outline',
+      })
+    },
+    size: 'large',
+  },
+  {
+    _label: '密码',
+    clearable: false,
+    model: 'password',
+    placeholder: '密码',
+    prefixIcon(h) {
+      return h(resolveComponent('icon'), {
+        icon: 'mdi:password-outline',
+      })
+    },
+    size: 'large',
+    type: 'password',
+  },
+  {
+    component: 'TCheckbox',
+    label: '记住密码',
+    model: 'rememberMe',
+  },
+  {
+    slot: 'submitBtn',
+  },
+]
+</script>
+
 <template>
-  <div class="h-full p-4">
-    <ListTable></ListTable>
-  </div>
+  <LoginBg>
+    <GradientText
+      :text="appName"
+      :animation-speed="8"
+      :show-border="false"
+      :colors="['#000000', '#444951', '#8791a1']"
+      class-name="text-center text-2xl !font-bold mt-8"
+    />
+    <TForm class="!mt-8" :data="formData" :items="formItems" @submit="onSubmit">
+      <template #submitBtn>
+        <TButton type="submit" block size="large" class="!mt-4">登录</TButton>
+      </template>
+    </TForm>
+  </LoginBg>
 </template>
--- src/views/system/menu/index.vue
+++ src/views/system/menu/index.vue
@@ -1,3 +1,3 @@
 <template>
-  <ListTable></ListTable>
+  <div></div>
 </template>
