16c119a3ab4e01f9f3ece58a5648a55cce58bc13
peterhu <peter@qcznai.com>
2026-01-12 16:33:22 +0800

    解决ts 报错

--- src/plugins/autoImport/_.ts
+++ src/plugins/autoImport/_.ts
@@ -1,8 +1,15 @@
 import { cloneDeep, groupBy, kebabCase, uniqueId } from 'lodash-es'
 
-export default {
+const _: {
+  cloneDeep: typeof cloneDeep
+  groupBy: typeof groupBy
+  kebabCase: typeof kebabCase
+  uniqueId: typeof uniqueId
+} = {
   cloneDeep,
   groupBy,
   kebabCase,
   uniqueId,
 }
+
+export default _
