0de9be2a69451d8c5a22b6550d9c78a7d383f603
peterhu <peter@qcznai.com>
2026-01-12 16:09:49 +0800

    PageQuery 计算表单项的折叠与展开

--- src/components/autoImport/PageList/PageQuery.vue
+++ src/components/autoImport/PageList/PageQuery.vue
@@ -35,6 +35,59 @@ const otherProps = computed(() => {
   return obj
 })
 const formInstance = useTemplateRef('formRef')
+// 计算哪些表单项需要展开/折叠 ------- start -------
+const showExtraFormItems = ref(false)
+const showToggleBtn = ref(false)
+let formDom: HTMLFormElement | null = null
+
+useResizeObserver(formInstance, (entries) => {
+  formDom = (entries[0]?.target ?? null) as HTMLFormElement | null
+  calcFormItemToggle()
+})
+
+function calcFormItemToggle(): void {
+  nextTick(() => {
+    if (formDom === null) {
+      return
+    }
+
+    const formItemsDomList: HTMLDivElement[] = Array.from(
+      formDom.querySelectorAll('.t-form__item') as NodeListOf<HTMLDivElement>, // display: none 的 offsetTop 为 0
+    )
+
+    formItemsDomList.forEach((item) => {
+      item.style.display = getComputedStyle(formItemsDomList[0]!).display
+    })
+
+    const co = _.groupBy(formItemsDomList, (item) => {
+      return item.offsetTop
+    })
+    const keys = Object.keys(co).sort((a, b) => Number(a) - Number(b))
+
+    showToggleBtn.value =
+      keys.length > 3 ||
+      (keys.length === 3 &&
+        Number(keys[keys.length - 1]) - Number(keys[keys.length - 2]) >
+          Number(keys[1]) - Number(keys[0]))
+
+    if (!showExtraFormItems.value) {
+      keys.slice(2, -1).forEach((key) => {
+        const items = co[key]
+
+        items!.forEach((item) => {
+          item.style.display = 'none'
+        })
+      })
+
+      if (keys.length > 2 && showToggleBtn.value) {
+        co[keys[1]!]![co[keys[1]!]!.length - 1]!.style.display = 'none'
+      }
+    }
+  })
+}
+
+watch([showExtraFormItems, () => formItems.value.length], calcFormItemToggle)
+// 计算哪些表单项需要展开/折叠 ------- end -------
 
 async function handleQuery() {
   await formInstance.value!.validate()
@@ -58,6 +111,16 @@ defineExpose({
     <template #btn>
       <TButton @click="handleQuery">查询</TButton>
       <TButton theme="default" @click="handleReset">重置</TButton>
+      <TButton
+        v-if="showToggleBtn"
+        variant="outline"
+        @click="showExtraFormItems = !showExtraFormItems"
+      >
+        展开
+        <template #icon>
+          <Icon :icon="showExtraFormItems ? 'ep:arrow-up' : 'ep:arrow-down'"></Icon>
+        </template>
+      </TButton>
     </template>
   </TForm>
 </template>
