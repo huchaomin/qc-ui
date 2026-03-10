0d9e291f0ff500d5e18c924ac7643693283b71c1
huchaomin <iisa_peter@163.com>
2026-03-10 17:00:04 +0800

    手动拉取记录

---
+++ src/views/dataCenter/commentHandPullRecord/index.vue
@@ -0,0 +1,105 @@
+<script setup lang="ts">
+const router = useRouter()
+const formItemMap = {
+  funcType: {
+    _label: '功能类型',
+    component: 'TSelect',
+    dicCode: 'func_type',
+    model: 'funcType',
+  },
+  status: {
+    _label: '状态',
+    component: 'TSelect',
+    dicCode: 'pull_status',
+    model: 'status',
+  },
+} satisfies Record<string, FormItem>
+const config: PageListProps = {
+  apis: {
+    delete: {
+      method: 'yq/commentHandPullRecord',
+      permission: 'yq:commentHandPullRecord:remove',
+    },
+    list: {
+      method: 'yq/commentHandPullRecord/list',
+    },
+  },
+  columns: [
+    {
+      cell: {
+        _component: 'DicLabel',
+        dicCode: 'func_type',
+      },
+      colKey: 'funcType',
+      title: '功能类型',
+    },
+    {
+      cell: (_, { row }) => {
+        const { funcId, funcType } = row
+        let routeName = ''
+
+        if (funcType === 2 && router.hasRoute('FollowDetail')) {
+          routeName = 'FollowDetail'
+        }
+
+        if (funcType === 3 && router.hasRoute('EventDetail')) {
+          routeName = 'EventDetail'
+        }
+
+        if (routeName !== '') {
+          return {
+            _component: 'Link',
+            onClick: () => {
+              router.push({
+                name: routeName,
+                query: {
+                  id: funcId,
+                },
+              })
+            },
+          }
+        }
+
+        return {
+          _component: 'Default',
+        }
+      },
+      colKey: 'funcId',
+      title: '功能id',
+    },
+    {
+      colKey: 'contentIds',
+      title: '内容id列表',
+    },
+    {
+      cell: {
+        _component: 'DicLabel',
+        dicCode: 'status',
+      },
+      colKey: 'status',
+      title: '状态',
+    },
+    {
+      colKey: 'createBy',
+      title: '创建人',
+    },
+    {
+      colKey: 'createTime',
+      title: '创建时间',
+    },
+    {
+      colKey: 'updateBy',
+      title: '更新人',
+    },
+    {
+      colKey: 'updateTime',
+      title: '更新时间',
+    },
+  ],
+  formItems: [formItemMap.funcType, formItemMap.status],
+}
+</script>
+
+<template>
+  <PageList v-bind="config"></PageList>
+</template>
