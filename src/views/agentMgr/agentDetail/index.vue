c8abfddf6fb24c3e9f471a01da65d8dde393e5b8
huchaomin <iisa_peter@163.com>
2026-03-11 18:06:09 +0800

    智能体详情

--- src/views/agentMgr/agentDetail/index.vue
+++ src/views/agentMgr/agentDetail/index.vue
@@ -15,10 +15,15 @@ const type = computed(() => {
 
   return 'view'
 })
-const formData = reactive<Record<string, any>>({
-  setup: 'enter',
+const data1 = reactive<Record<string, any>>({
   status: '0',
+})
+const data2 = reactive<Record<string, any>>({
+  setup: 'enter',
   temperature: 0.1,
+})
+const data3 = reactive<Record<string, any>>({
+  output: '文件Excel',
 })
 const formItems: FormItem[] = [
   {
@@ -62,7 +67,7 @@ const { data: modelOptions, onSuccess } = useRequest(
 
 onSuccess(() => {
   if (type.value === 'add') {
-    formData.modelId = modelOptions.value[0]?.value ?? ''
+    data2.modelId = modelOptions.value[0]?.value ?? ''
   }
 })
 
@@ -101,31 +106,199 @@ const formItems2: FormItem[] = [
   },
 ]
 const formItems3: FormItem[] = [
-  {
+  reactive({
+    _rules: [
+      {
+        message: '请输入规则设定',
+        required: true,
+      },
+    ],
+    class: 'flex_height',
     component: 'TTextarea',
     maxlength: '',
     model: 'promptContent',
     placeholder: '你是一个文案撰写专家',
-  },
+    show: computed(() => data2.setup === 'enter'),
+  }),
+  reactive({
+    _label: '角色',
+    component: 'TTextarea',
+    maxlength: '',
+    model: 'role',
+    placeholder: '你是一个文案撰写专家',
+    show: computed(() => data2.setup === 'expand'),
+  }),
+  reactive({
+    _label: '背景',
+    component: 'TTextarea',
+    maxlength: '',
+    model: 'background',
+    placeholder: '你服务于-家AI公司，主要面向企业开发AI应用',
+    show: computed(() => data2.setup === 'expand'),
+  }),
+  reactive({
+    _label: '技能',
+    component: 'TTextarea',
+    maxlength: '',
+    model: 'skills',
+    placeholder: '查询知识库/查询文件:当用户咨询问题时，必须使用此技能',
+    show: computed(() => data2.setup === 'expand'),
+  }),
+  reactive({
+    _label: '任务',
+
+    component: 'TTextarea',
+    maxlength: '',
+    model: 'tasks',
+    placeholder: '从知识库中寻找最佳答案，解答客户问题',
+    show: computed(() => data2.setup === 'expand'),
+  }),
+  reactive({
+    _label: '要求',
+    component: 'TTextarea',
+    maxlength: '',
+    model: 'requirements',
+    placeholder: '一步一步思考:认真检查你的回答是否可以解决用户的问题',
+    show: computed(() => data2.setup === 'expand'),
+  }),
+  reactive({
+    _label: '输出格式',
+    component: 'TRadioGroup',
+    model: 'output',
+    options: [
+      {
+        label: '文件Excel',
+        value: '文件Excel',
+      },
+      {
+        label: '文本输出',
+        value: '文本输出',
+      },
+    ],
+    show: computed(() => data2.setup === 'expand'),
+  }),
 ]
+const cardRef = useTemplateRef('cardRef')
+const { height: _height } = useElementSize(
+  () => cardRef.value?.$el.querySelector('.t-card__body') as HTMLElement,
+)
+const height = computed(() => (type.value === 'view' ? _height.value - 16 : _height.value - 52))
+const form1Ref = useTemplateRef('form1Ref')
+const form2Ref = useTemplateRef('form2Ref')
+const form3Ref = useTemplateRef('form3Ref')
+
+function getPromptContent(formData: Record<string, any>): string {
+  let str = ''
+
+  ;['background', 'skills', 'tasks', 'requirements'].forEach((key) => {
+    str += `${formData[key as keyof typeof formData]}\n`
+  })
+  str += `输出格式: ${formData.output}`
+  return str
+}
+
+async function handleSave(): Promise<void> {
+  const [form1Data, form2Data, form3Data] = await Promise.all([
+    form1Ref.value!.validate(),
+    form2Ref.value!.validate(),
+    form3Ref.value!.validate(),
+  ])
+  const data: Record<string, any> = {
+    ...form1Data,
+    ...form2Data,
+    brandId,
+    promptContent: form3Data.promptContent ?? getPromptContent(form3Data),
+    promptContentJson: form2Data.setup === 'expand' ? JSON.stringify(form3Data) : '',
+    promptRole: form3Data.role ?? '',
+  }
+
+  delete data.setup
+
+  if (type.value === 'add') {
+    await alovaInst.Post('yq/agentManage', data)
+    $msg('新增智能体成功')
+  } else {
+    await alovaInst.Put('yq/agentManage', {
+      ...data,
+      id,
+    })
+    $msg('修改智能体成功')
+  }
+
+  useExcludeKPnameStore().add('AgentList')
+  useRecentRoutersStore().close(route.name)
+}
+
+const { data: getAgentByIdData, onSuccess: onGetAgentByIdSuccess } = useRequest(
+  alovaInst.Get<Record<string, any>>(`yq/agentManage/${id}`),
+  {
+    immediate: !isFalsy(id),
+    initialData: [],
+  },
+)
+
+onGetAgentByIdSuccess(() => {
+  form1Ref.value!.setFormData(getAgentByIdData.value)
+  form2Ref.value!.setFormData(getAgentByIdData.value)
+  data2.setup = isFalsy(getAgentByIdData.value.promptContentJson) ? 'enter' : 'expand'
+  data3.promptContent = getAgentByIdData.value.promptContent
+
+  if (data2.setup === 'expand') {
+    try {
+      form3Ref.value!.setFormData(JSON.parse(getAgentByIdData.value.promptContentJson))
+    } catch (error) {
+      console.error(error)
+    }
+  }
+})
 </script>
 
 <template>
-  <div>
+  <div class="flex flex-col gap-4!">
     <TCard :title="`品牌：${useListLabel('brand', brandId).value}`">
-      <TForm :data="formData" :items="formItems"></TForm>
+      <TForm ref="form1Ref" :data="data1" :items="formItems" :disabled="type === 'view'"></TForm>
     </TCard>
-    <TCard class="mt-4!" title="规则">
+    <TCard
+      ref="cardRef"
+      title="规则"
+      class="flex-1"
+      :body-class-name="`flex! flex-col min-h-[150px] ${data2.setup === 'enter' ? 'justify-end relative' : 'justify-between'}`"
+      body-full-height
+    >
       <template #actions>
         <TForm
+          ref="form2Ref"
           label-align="right"
           :auto-label-width="false"
           class="no_item_mb flex!"
-          :data="formData"
+          :data="data2"
           :items="formItems2"
+          :disabled="type === 'view'"
         ></TForm>
       </template>
-      <TForm layout="vertical" :data="formData" :items="formItems3"></TForm>
+      <TForm
+        ref="form3Ref"
+        :class="{
+          'absolute top-4 right-6 left-6': data2.setup === 'enter',
+        }"
+        layout="vertical"
+        :data="data3"
+        :items="formItems3"
+        :disabled="type === 'view'"
+      ></TForm>
+      <TButton v-if="type !== 'view'" block @click="handleSave"> 保存 </TButton>
     </TCard>
   </div>
 </template>
+
+<style scoped>
+:deep() {
+  .t-textarea {
+    &.flex_height {
+      textarea {
+        height: calc(v-bind(height) * 1px) !important;
+      }
+    }
+  }
+}
+</style>
