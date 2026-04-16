434cfbe0b59b4d54407cd205cf2ae12cb35e79a6
huchaomin <iisa_peter@163.com>
2026-04-16 10:39:01 +0800

    tabs > card 阴影溢出

--- src/views/dataCenter/brandVideoList/videoDetail/modules/AnalysisResults.vue
+++ src/views/dataCenter/brandVideoList/videoDetail/modules/AnalysisResults.vue
@@ -6,7 +6,7 @@ const data = inject<Ref<Record<string, any> | undefined>>('data')!
 </script>
 
 <template>
-  <div class="mb-2 flex flex-col gap-4">
+  <div class="mx-1 mb-2 flex flex-col gap-4">
     <MoodAnalysisAndContentSummarize :analysis-result="data"></MoodAnalysisAndContentSummarize>
     <ContentSlice v-if="data !== undefined" :data="data"></ContentSlice>
   </div>
