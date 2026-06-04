129c93532b8d3606a866ed4637006b72c5509ab3
huchaomin <iisa_peter@163.com>
2026-06-04 17:03:16 +0800

    操作 列 button 过多的问题

--- src/plugins/tableRenders/cell/Buttons.vue
+++ src/plugins/tableRenders/cell/Buttons.vue
@@ -13,21 +13,50 @@ export interface ButtonsProps {
 }
 
 const attrs = useAttrs() as unknown as CellRenderContext
+const finallyButtons = computed(() => {
+  return props.buttons
+    .map((item) => {
+      return typeof item === 'function' ? item(attrs) : item
+    })
+    .filter((item) => item.show !== false && checkPermissions(item.permission))
+})
 </script>
 
 <template>
   <div class="flex">
     <TButton
-      v-for="(child, index) in props.buttons"
+      v-for="(child, index) in finallyButtons.slice(0, finallyButtons.length === 4 ? 4 : 3)"
       :key="index"
       v-bind="{
         theme: 'primary',
         variant: 'text',
-        ...(typeof child === 'function' ? child(attrs) : child),
+        ...child,
       }"
     >
       {{ _get(attrs.row, attrs.col.colKey) }}
     </TButton>
+    <TPopup v-if="finallyButtons.length > 4">
+      <TButton shape="square" theme="primary" variant="text">
+        <template #icon>
+          <Icon icon="ri:more-fill" />
+        </template>
+      </TButton>
+      <template #content>
+        <div>
+          <TButton
+            v-for="(child, index) in finallyButtons.slice(3)"
+            :key="index"
+            v-bind="{
+              theme: 'primary',
+              variant: 'text',
+              ...child,
+            }"
+          >
+            {{ _get(attrs.row, attrs.col.colKey) }}
+          </TButton>
+        </div>
+      </template>
+    </TPopup>
   </div>
 </template>
 
